cb320aa68b10eecd8f22dde9630ff5200308fa7b
Anton Shcherbinin <shcherbinin.anton.v@gmail.com>
2022-11-29 08:39:50 +0300

    3.2: update test

--- 3-functions/2-recursion.test.js
+++ 3-functions/2-recursion.test.js
@@ -2,12 +2,12 @@ import { describe, expect, test } from "@jest/globals";
 import { isEven } from "./2-recursion";
 
 describe("3-functions/2-recursion.js", () => {
-  test("2 is even", () => {
-    expect(isEven(2)).toBeTruthy();
+  test("50 is even", () => {
+    expect(isEven(50)).toBeTruthy();
   });
 
-  test("17 is NOT even", () => {
-    expect(isEven(-17)).toBeFalsy();
+  test("75 is NOT even", () => {
+    expect(isEven(75)).toBeFalsy();
   });
 
   test("-1 is NOT even", () => {
